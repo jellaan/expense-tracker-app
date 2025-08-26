60399cad117861dddb78b5f752ba9c4ac5ac08e0
jellaan <jahn3@alaska.edu>
2025-08-26 16:54:13 +0200

    feat(header): add a header button and make expense items tappable

--- App.js
+++ App.js
@@ -10,6 +10,7 @@ import AllExpenses from './screens/AllExpenses';
 import RecentExpenses from './screens/RecentExpenses';
 
 import { GlobalStyles } from './constants/styles';
+import IconButton from './components/UI/IconButton';
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
@@ -21,7 +22,8 @@ function ExpensesOverview() {
         headerStyle: { backgroundColor: GlobalStyles.colors.primary500 },
         headerTintColor: 'white',
         tabBarStyle: { backgroundColor: GlobalStyles.colors.primary500 },
-        tabBarActiveTintColor: GlobalStyles.colors.accent500
+        tabBarActiveTintColor: GlobalStyles.colors.accent500,
+        headerRight: ({tintColor}) => <IconButton icon="add" size={24} color={tintColor} onPress={() => {}} />
       }}
     >
       <Tab.Screen name="AllExpenses" component={AllExpenses} 
