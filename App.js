5658252c598dcbecbcbee7aa9b5b5819d088c3d4
jellaan <jahn3@alaska.edu>
2025-08-26 20:28:32 +0200

    feat(navigation): set a navigation between screens (ManageExpense)

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
@@ -17,32 +17,32 @@ const Tab = createBottomTabNavigator();
 
 function ExpensesOverview() {
   return (
-    <Tab.Navigator 
-      screenOptions={{
+    <Tab.Navigator
+      screenOptions={({ navigation }) => ({
         headerStyle: { backgroundColor: GlobalStyles.colors.primary500 },
         headerTintColor: 'white',
         tabBarStyle: { backgroundColor: GlobalStyles.colors.primary500 },
         tabBarActiveTintColor: GlobalStyles.colors.accent500,
-        headerRight: ({tintColor}) => <IconButton icon="add" size={24} color={tintColor} onPress={() => {}} />
-      }}
+        headerRight: ({ tintColor }) => <IconButton icon="add" size={24} color={tintColor} onPress={() => { navigation.navigate('ManageExpense') }} />
+      })}
     >
-      <Tab.Screen name="AllExpenses" component={AllExpenses} 
-        options={{ 
-          title: 'All Expenses', 
-          tabBarLabel: 'All', 
+      <Tab.Screen name="AllExpenses" component={AllExpenses}
+        options={{
+          title: 'All Expenses',
+          tabBarLabel: 'All',
           tabBarIcon: ({ color, size }) => (
             <Ionicons name="hourglass" size={size} color={color} />
           )
-        }} 
+        }}
       />
-      <Tab.Screen name="RecentExpenses" component={RecentExpenses} 
-        options={{ 
-          title: 'Recent Expenses', 
-          tabBarLabel: 'Recent', 
+      <Tab.Screen name="RecentExpenses" component={RecentExpenses}
+        options={{
+          title: 'Recent Expenses',
+          tabBarLabel: 'Recent',
           tabBarIcon: ({ color, size }) => (
             <Ionicons name="calendar" size={size} color={color} />
           )
-        }} 
+        }}
       />
     </Tab.Navigator>
   );
@@ -55,7 +55,7 @@ export default function App() {
       <StatusBar style="auto" />
       <NavigationContainer>
         <Stack.Navigator>
-          <Stack.Screen name="ExpensesOverview" component={ExpensesOverview} options={{ headerShown: false }}/>
+          <Stack.Screen name="ExpensesOverview" component={ExpensesOverview} options={{ headerShown: false }} />
           <Stack.Screen name="ManageExpense" component={ManageExpense} />
         </Stack.Navigator>
       </NavigationContainer>
--- components/ExpensesOutput/ExpenseItem.js
+++ components/ExpensesOutput/ExpenseItem.js
@@ -1,15 +1,18 @@
 import { Pressable, StyleSheet, Text, View } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
 import { GlobalStyles } from '../../constants/styles';
 import { getFormattedDate } from '../../util/date';
 
 function ExpenseItem({ description, amount, date }) {
-    function expensePressHandler() {
+  const navigation = useNavigation();
 
-    }
+  function expensePressHandler() {
+    navigation.navigate('ManageExpense');
+  }
 
   return (
-    <Pressable onPress={expensePressHandler} style={({pressed}) => pressed && styles.pressed}>
+    <Pressable onPress={expensePressHandler} style={({ pressed }) => pressed && styles.pressed}>
       <View style={styles.expenseItem}>
         <View>
           <Text style={[styles.textBase, styles.description]}>
@@ -28,9 +31,9 @@ function ExpenseItem({ description, amount, date }) {
 export default ExpenseItem;
 
 const styles = StyleSheet.create({
-    pressed: {
-        opacity: 0.75
-    },  
+  pressed: {
+    opacity: 0.75
+  },
   expenseItem: {
     padding: 12,
     marginVertical: 8,
